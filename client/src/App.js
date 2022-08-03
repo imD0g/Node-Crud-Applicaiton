a1837449758a7c87ba127bb20c6268929ae8f877
imD0g <alphagamingcommunity@gmail.com>
2022-08-03 22:18:11 +0100

    im tired now, most up to date change

--- client/src/App.js
+++ client/src/App.js
@@ -14,10 +14,12 @@ function App() {
      const [position, setPosition] = useState("");
      const [wage, setWage] = useState(0);
 
+     const [employeeList, setEmployeeList] = useState([]);
+
      // This is the function that is called when the user submits the form.
      // It takes the values from the form and sends them to the server.
      // Axios.post is sending the data to the server.
-     // Axios.post (url, data (we are sending an object with the data))
+     // Axios.post (url, data (we are sending an object with the data)).
      const addEmployee = () => {
           Axios.post("http://localhost:3001/create", {
                name: name, // Key: value
@@ -29,6 +31,7 @@ function App() {
                console.log("Success");
           });
      };
+
      return (
           <div className="App">
                <div className="information">
@@ -77,6 +80,10 @@ function App() {
 
                     {/* Submit Button */}
                     <button onClick={addEmployee}>Add Employee</button>
+               </div>
+               <br></br> {/* This is a break tag */}
+               <div className="employees">
+                    <button>Show employees in the Database</button>
                </div>
           </div>
      );
