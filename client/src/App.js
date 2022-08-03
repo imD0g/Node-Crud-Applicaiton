7f8dc81f81ab33ddd01ad3cb21d198966634b9a3
imD0g <alphagamingcommunity@gmail.com>
2022-08-03 21:34:42 +0100

    Added backend and fontend functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,82 @@
 import "./App.css";
+import { useState } from "react";
+import Axios from "axios";
+
+// This is the main component of the app:
 
 function App() {
+     // Here we are using the useState hook to create a state variable.
+     // State is a piece of data that is stored in the component.
+
+     const [name, setName] = useState("");
+     const [age, setAge] = useState(0);
+     const [country, setCountry] = useState("");
+     const [position, setPosition] = useState("");
+     const [wage, setWage] = useState(0);
+
+     // This is the function that is called when the user submits the form.
+     // It takes the values from the form and sends them to the server.
+     // Axios.post is sending the data to the server.
+     // Axios.post (url, data (we are sending an object with the data))
+     const addEmployee = () => {
+          Axios.post("http://localhost:3001/create", {
+               name: name, // Key: value
+               age: age,
+               country: country,
+               position: position,
+               wage: wage,
+          }).then(() => {
+               console.log("Success");
+          });
+     };
      return (
           <div className="App">
                <div className="information">
+                    {/* Name Input */}
                     <label>Name:</label>
-                    <input type="text" />
+                    <input
+                         type="text"
+                         onChange={(event) => {
+                              setName(event.target.value);
+                         }}
+                    />
+                    {/* Age Input */}
                     <label>Age:</label>
-                    <input type="number" />
+                    <input
+                         type="number"
+                         onChange={(event) => {
+                              setAge(event.target.value);
+                         }}
+                    />
+                    {/* Country Input */}
                     <label>Country:</label>
-                    <input type="text" />
+                    <input
+                         type="text"
+                         onChange={(event) => {
+                              setCountry(event.target.value);
+                         }}
+                    />
+
+                    {/* Position Input */}
                     <label>Position:</label>
-                    <input type="text" />
+                    <input
+                         type="text"
+                         onChange={(event) => {
+                              setPosition(event.target.value);
+                         }}
+                    />
+
+                    {/* Wage Input */}
                     <label>Wage (Yearly):</label>
-                    <input type="number" />
-                    <button>Add Employee</button>
+                    <input
+                         type="number"
+                         onChange={(event) => {
+                              setWage(event.target.value);
+                         }}
+                    />
+
+                    {/* Submit Button */}
+                    <button onClick={addEmployee}>Add Employee</button>
                </div>
           </div>
      );
---
+++ server/index.js
@@ -0,0 +1,55 @@
+// Here we import what we need to use in this file
+const express = require("express"); // import express
+const app = express(); // create an instance of express
+const mysql = require("mysql"); // import mysql
+const cors = require("cors"); // import cors
+
+app.use(cors()); // enable cors
+app.use(express.json());
+
+// create a connection to the database
+const connection = mysql.createConnection({
+     user: "root",
+     host: "localhost",
+     password: "P@55word",
+     database: "employee-system",
+});
+
+// Post request to add a new employee to the database
+//
+// Here we are creating variables to send to the database
+// app.post is a method that takes a path and a callback function
+app.post("/create", (req, res) => {
+     const name = req.body.name;
+     const age = req.body.age;
+     const country = req.body.country;
+     const position = req.body.position;
+     const wage = req.body.wage;
+
+     //
+     // Here we are using the connection object to execute a query in MySQL:
+     // the ? is a placeholder for the values that will be inserted into the database (We will pass in an array of values)
+     // We then pass in an array which contains the values that will be inserted into the database (in this case the values from the form)
+     // (err, results) is a callback function - What will we do when the fucntion is done?
+     // "err" is the error, "result" is the result of the query
+     //
+     connection.query(
+          "INSERT INTO employees (name, age, country, position, wage) VALUES (?, ?, ?, ?, ?)",
+          [name, age, country, position, wage],
+          (err, results) => {
+               if (err) {
+                    // If there is an error, send the error to the client
+                    console.log(err);
+                    res.sendStatus(500);
+               } else {
+                    // If there is no error, send a success message to the client
+                    res.sendStatus(200);
+               }
+          }
+     );
+});
+
+// launch server on port 3001
+app.listen(3001, () => {
+     console.log("Server listening on port 3001");
+});
